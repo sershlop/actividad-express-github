0ddac17c9292a15640d711155af3ce9de63d73d0
Sergio Gutiérrez López <sergio>
2026-03-25 23:06:58 -0700

    integracion de postgreSQL con Express

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'sergio',
+  host: 'localhost',
+  database: 'react_express_db',
+  password: 'oigres40152@',
+  port: 5432,
+});
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('./db');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -11,9 +12,16 @@ app.get('/usuario', (req, res) => {
     nombre: 'Sergio Gutierrez Lopez',
     rol: 'Estudiante'
   };
-
   res.json(usuario);
 });
+
+pool.connect()
+  .then(() => {
+    console.log('Conexión exitosa a PostgreSQL');
+  })
+  .catch((err) => {
+    console.error('Error de conexión', err);
+  });
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
