c5bc893cec730946075af1d50919cc51cb0c7c60
Sergio Gutiérrez López <sergio>
2026-04-14 23:29:09 -0700

    agrega ruta GET/alumnos con conexion a PostgresSQL

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const pool = require('./db');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
-app.get('/usuario', (req, res) => {
-  const usuario = {
-    id: 1,
-    nombre: 'Sergio Gutierrez Lopez',
-    rol: 'Estudiante'
-  };
-  res.json(usuario);
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
 });
 
 pool.connect()
