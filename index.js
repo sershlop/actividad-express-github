fe1e57e6df38cce4382befa11715c8b862351710
Sergio Gutiérrez López <sergio>
2026-04-30 00:40:41 -0700

    actualizacion con integracion para recibir el parametro id

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ app.get('/alumnos', async (req, res) => {
     res.status(500).json({ error: 'Error al obtener los alumnos' });
   }
 });
+// Obtener un alumno por ID
+app.get('/alumnos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const resultado = await pool.query('SELECT * FROM alumno WHERE id = $1', [id]);
+    if (resultado.rows.length === 0) {
+      return res.status(404).json({ error: 'Alumno no encontrado' });
+    }
+    res.json(resultado.rows[0]);
+  } catch (error) {
+    console.error('Error al consultar alumno:', error);
+    res.status(500).json({ error: 'Error al obtener el alumno' });
+  }
+});
+
+
 
 app.post('/alumnos', async (req, res) => {
   try {
@@ -58,6 +74,32 @@ app.post('/materia', async (req, res) => {
   }
 });
 
+
+app.get('/materias', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const resultado = await pool.query('SELECT * FROM materia WHERE id = $1', [id]);
+    if (resultado.rows.length === 0) {
+      return res.status(404).json({ error: 'Materia no encontrada' });
+    }
+    res.json(resultado.rows[0]);
+  } catch (error) {
+    console.error('Error al consultar materia:', error);
+    res.status(500).json({ error: 'Error al obtener la materia' });
+  }
+});
+
+app.get('/materia', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM materia');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar materias:', error);
+    res.status(500).json({ error: 'Error al obtener las materias' });
+  }
+});
+
+
 pool.connect()
   .then(() => console.log('Conexión exitosa a PostgreSQL'))
   .catch((err) => console.error('Error de conexión', err));
@@ -65,3 +107,7 @@ pool.connect()
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
+
+if (isNaN(id)) {
+  return res.status(400).json({ error: 'ID debe ser un número' });
+}
